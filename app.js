bbc0399fe4f59d0d50d689c4ad6ef4272363c24b
olsikule <olsi.kule@gmail.com>
2022-09-24 11:31:57 -0400

    worked on updating javascript

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 // pseudocode
 
-// when the doted icon in clicked 
+// when the doted icon is clicked 
 // navbar menu slides to the right
 // when the x is clicked 
 // the navbar menu slides to the leght
 
+// window.onload = function (){
+
+        const openNav = document.querySelector("input");
+        const closeNav = document.querySelector("button");
+
+        closeNav.addEventListener("click", function() {
+            console.log("this element has been clicked")
+            // openNav.classList.toggle("check");
+            // closeNav.classList.toggle("checked");
+        });
+        // const navigation = function (openNav) {
+        // return("this element has been clicked")
+        //  }
+        // const closeNav = document.querySelector(".check");
+
+        
+
+    // }
 
 
-const openNav = document.querySelector(".slideOutNav");
-const closeNav = document.querySelector(".check");
 
-openNav.addEventListener("click", () => {
-    openNav.classList.toggle("checkbox");
-    closeNav.classList.toggle("checkbox");
-});
+    // Notes:
+    // HTML elements seem to work in querySelector method 
+    // but CSS selectors don't work - not sure why
+    // the x button to close navbar is not on the page to be able and experiment with console.log fxn
