7f9ca1d1de40ee201122ea99ad84c0327363463a
olsikule <olsi.kule@gmail.com>
2022-09-25 13:44:29 -0400

    updated functionality of open-close sidebar nav in JS

--- app.js
+++ app.js
@@ -5,24 +5,20 @@
 // when the x is clicked 
 // the navbar menu slides to the leght
 
-// window.onload = function (){
 
-        const openNav = document.querySelector("input");
-        const closeNav = document.querySelector("button");
 
-        closeNav.addEventListener("click", function() {
-            console.log("this element has been clicked")
-            // openNav.classList.toggle("check");
-            // closeNav.classList.toggle("checked");
-        });
-        // const navigation = function (openNav) {
-        // return("this element has been clicked")
-        //  }
-        // const closeNav = document.querySelector(".check");
+        const openNav = document.querySelector("#openSidebarMenu");
+        const closeNav = document.querySelector(".closeButton");
+        
+        closeNav.addEventListener("click", function(){
+            // console.log("this element has been clicked");
+            openNav.checked = false;
+        })
+     
 
         
 
-    // }
+    
 
 
 
@@ -30,3 +26,8 @@
     // HTML elements seem to work in querySelector method 
     // but CSS selectors don't work - not sure why
     // the x button to close navbar is not on the page to be able and experiment with console.log fxn
+
+    // I've narrowed this down to the css selector that is responsive for nav slideout 
+    // what do I need to do next ? 
+    // I've disabled the css declaration and will try to work with js until I find a response on the browser
+    // to find out if js code works I'm assigning the open fxn to another icon on the page like search icon
